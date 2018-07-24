c001ddca2d8048d2bdd843d3db37c30d78cb369a
dima.golyak <dima.golyak@idapgroup.com>
2018-07-24 10:13:20 +0300

    added article list

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from '../article.model';
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.css']
 })
 export class ArticleListComponent implements OnInit {
-
+  @Input() articles: Article[];
   constructor() { }
 
   ngOnInit() {
