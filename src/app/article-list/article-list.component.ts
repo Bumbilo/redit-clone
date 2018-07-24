0075b5dcc23607bfdbf73e65f1ecc5edc0f1a043
Bumbilo <dimagolyak@gmail.com>
2018-07-24 10:59:26 +0300

    added method in article service

--- src/app/article-list/article-list.component.ts
+++ src/app/article-list/article-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../article.model';
+import { ArticleService } from '../article.service';
+
 @Component({
   selector: 'app-article-list',
   templateUrl: './article-list.component.html',
   styleUrls: ['./article-list.component.css']
 })
+
 export class ArticleListComponent implements OnInit {
   @Input() articles: Article[];
-  constructor() { }
+
+  constructor(private articleService: ArticleService) {
+  }
 
   ngOnInit() {
+    this.articleService
+      .getArticles()
+      .then(articles => this.articles = articles);
   }
 
 }
--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Article } from './article.model';
 
 @Injectable()
 export class ArticleService {
 
   constructor() { }
 
+  public getArticles(): Promise<Article[]> {
+    return new Promise(resolve => {
+        setTimeout(() => {
+          resolve([
+            new Article('The Angular 2 screencast', 'The easiest way to learn Angular 2', 10),
+            new Article('Real React', 'React 6', 6),
+            new Article('View', 'I dont know view', 2),
+            new Article('Learn Jquery', 'Jquery not 6', 3),
+            new Article('Underscore', 'I dont know Underscore', 4),
+            new Article('Real Life is good', 'Good is good like this', 3),
+            new Article('View is not a good', 'asdlfnow view', 1),
+        ]);
+      }, 2000);
+    });
+  }
 }
